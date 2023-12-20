2eb21729e0fa0710257c1e9e93fef3d83a766873
Mukhtar Mukhtar Mukhtar <Mukhtarmb98@gmail.com>
2023-12-20 13:56:57 +0100

    operator"s assignment

---
+++ Mukhtar/operators.py
@@ -0,0 +1,18 @@
+# Q1. Create a list of fruits
+fruits = ['mangoes' , 'apples' , 'grapes' , 'pawpaws' , 'oranges']
+# Q2. Using the right opera'tor check if your list contains 'apple'. Your answer should be valid regardless of casing.
+print('apples' in fruits)
+# Q3. Remove the last item from the list and check if it contains the alphabet 'a'
+k = fruits[4]   
+print( k,'a'in k )
+
+# Q4. Create a variable to hold the value 5
+x= 90
+# Q6. Add 5 to the above variable
+x+= 10
+# Q7. Multply it by 6
+x*= 6
+# Q8. Check if the final value is less than 100 or 
+print(x>100)
+# if the list of fruits doesnt contain banana
+print('banana' in fruits)
