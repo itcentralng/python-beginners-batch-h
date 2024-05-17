b0222f6935bcda80a85d01f441be6d89b07d78dd
Nasiru Mustapha <34660309+mrteey@users.noreply.github.com>
2024-05-17 12:22:32 +0100

    Update classwork.py

--- assignments/classwork.py
+++ assignments/classwork.py
@@ -11,3 +11,36 @@
 # the most appropraite data types.
 # Q10. Store a value for the student in the dictionary that indicates whether the student
 # is admitted or not using a boolean.
+
+
+# FUNCTIONS
+# Q1. Write a function that takes name and age 
+# and print "Hi my name is the-name-goes-here and I am the-age-goes-here years old"
+
+# Q2 Write a function that takes two lists e.g. list1=["name", "age", "height"]
+# list2 = ["Ahmad Sani", 15, 1.6] and 
+# returns a dictionary e.g. {"name":"Ahmad Sani", "age":15, "height":1.6}
+
+"""
+1. Write a fuction that returns the first two integers from
+    a list of integers. Using list slicing, get the new numbers
+    in reverse.
+
+2. Write a function that takes two names and retun them as
+    one seperated by space. Using the right string method,
+    check if the first name starts with upper case.
+"""
+
+"""
+3. Write a function that takes a number and return for example:
+    '2 is an even number' if 2 were to be passed to the function or
+    '3 is an odd numebr' if 3 were to be passed to the function.
+"""
+
+"""
+1. Write a function to calculate the average of a list of numbers?
+2. Write a function to determine whether a given number is an integer or a float?
+3. Write a function to convert temperatures between Celsius and Fahrenheit?
+4. Write a function to find the maximum or minimum value in a list of numbers?
+5. Write a function to sort a list of strings or numbers in ascending or descending order?
+"""
